1436c472c40a08aaa4f9faf228b10a30ee7703bf
ilhom <ilhamram332@gmail.com>
2026-06-09 15:46:18 +0700

    chore(finance): drop orphaned cost_notification_preference table (zero code references)

---
+++ services/finance/migrations/postgres/000370_drop_cost_notification_preference.up.sql
@@ -0,0 +1,3 @@
+-- cost_notification_preference was created in migration 000214 but is
+-- never referenced by any application code. Dropping to avoid schema drift.
+DROP TABLE IF EXISTS cost_notification_preference;
